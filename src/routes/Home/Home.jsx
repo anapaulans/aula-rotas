419e3c37ac50828c542b038eb65c4835b58f62a0
anapaulans <rm552513@fiap.com.br>
2023-09-13 18:56:07 -0300

    fazendo a pagina home

---
+++ src/routes/Home/Home.jsx
@@ -0,0 +1,24 @@
+export default function Home() {
+    return (
+      <main>
+        <div>
+          <section>
+            <h2>Promoções do dia</h2>
+            <article class="news-item">
+              <img src="/img celular.png" alt="Imagem do produto 1" />
+              <h3>Apple iPhone 13</h3>
+              <p>Apple iPhone 13 128GB Meia-noite Tela 6,1” 12MP</p>
+              <a href="#">R$ 4.099,00 no Pix</a>
+          </article>
+            <article class="news-item">
+              <img src="/img macbook.jpg" alt="Imagem do produto 2" />
+              <h3>MacBook Air 13</h3>
+              <p>Processador M1 da Apple com CPU 8‑core e GPU 7‑core, 8GB RAM, 256GB - Cinza Espacial</p>
+              <a href="#">R$ 7.639,00 no PIX</a>
+          </article>
+          </section>
+        </div>
+      </main>
+    )
+  }
+  
