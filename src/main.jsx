5258301e866352a8006c73656ffa9d860be4d11b
anapaulans <rm552513@fiap.com.br>
2023-09-13 19:01:15 -0300

    realizando o main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,45 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
 
+//importando elementos que irá precisar do router e as paginas main.jsx. E chamando os componentes Menu e Rodape no App
+import {createBrowserRouter, RouterProvider, Navigate} from 'react-router-dom'
+import Home from './routes/Home/Home.jsx'
+import Produtos from './routes/Produtos/Produtos.jsx'
+import Error from './routes/Error/Error.jsx'
+import EditarProduto from './routes/EditarProdutos/EditarProdutos.jsx'
+
+
+//Configurando rotas para utilizar o metodo createBrowserRouter, recebe dados do App e dentro do atributo children
+const router = createBrowserRouter([
+  {
+   path: '/',
+   element: <App/>,
+   errorElement: <Error/>,
+   children:[
+    {
+      path:"/",
+      element:<Home/>
+    },
+    {
+      path: "/produtos",
+      element:<Produtos />
+    },
+    {
+      path:"produtos/editar/:id",
+      element: <EditarProduto/>
+    },
+    {
+// Serve para se alguem tentar ir para o endereço antigo, será direcionado para a pagina home
+      path: "/antiga",
+      element: <Navigate to="/"/>
+    }
+  ]
+}])
+
+//Usa o RouterProvider para controlar  a renderização das páginas no ReactDOM, passando o router
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
