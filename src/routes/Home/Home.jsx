679162a4a1e30a97635fc9c4447c3bac09020eb8
anapaulans <rm552513@fiap.com.br>
2023-09-15 14:57:16 -0300

    organizando a pagina home

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -5,13 +5,13 @@ export default function Home() {
           <section>
             <h2>Promoções do dia</h2>
             <article class="news-item">
-              <img src="public/img celular.png" alt="Imagem do produto 1" />
+              <img src="https://m.media-amazon.com/images/I/51iJx7YWDOL.__AC_SY445_SX342_QL70_ML2_.jpg" alt="Imagem do produto 1" />
               <h3>Apple iPhone 13</h3>
               <p>Apple iPhone 13 128GB Meia-noite Tela 6,1” 12MP</p>
               <a href="#">R$ 4.099,00 no Pix</a>
           </article>
             <article class="news-item">
-              <img src="public/img macbook.jpg" alt="Imagem do produto 2" />
+              <img src="https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcRzWBkjEY1mZUmOKWWNdVfGFPcT26hvqA6ieHyJoEPxCEorcsVua-NhY90SyjELYsBxVjZZQA_keqZNAI7JUggtJK2J-jskasg700CGirwwOAScVNy9wcnP6A&usqp=CAE" />
               <h3>MacBook Air 13</h3>
               <p>Processador M1 da Apple com CPU 8 core e GPU 7 core, 8GB RAM, 256GB - Cinza Espacial</p>
               <a href="#">R$ 7.639,00 no PIX</a>
